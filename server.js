d2a16b5a39e305b00b277a52364cfeee7b0ea5f5
N1nji <pedro.felipe7911@gmail.com>
2025-10-22 23:48:32 -0300

    Configurando o CORS para permitir que o Vercel acesse a API

--- server.js
+++ server.js
@@ -8,7 +8,24 @@ import newsletterRoutes from "./routes/newsletterRoutes.js"
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+// Configuração de CORS
+const allowedOrigins = [
+  "https://portfolio-frontend-kappa-five.vercel.app", // frontend no Vercel
+  "http://localhost:5173",
+];
+
+app.use(cors({
+    origin: function(origin, callback) {
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){
+            const msg = `CORS policy: origin ${origin} not allowed`;
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
+    
 app.use(express.json());
 
 // Rotas
